6bfe6a65db8840053bda941c650f8c0377c9bd77
Daniel Firme <danielfirme.github@gmail.com>
2022-09-29 14:47:35 -0300

    Acrescentando novas imagens e melhorando o codigo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@
 $ = document.querySelector.bind(document); 
 $$ = document.querySelectorAll.bind(document);
 
-let currentSlide = 0;
-
 let totalSlides = $$('.slider--item').length;
 const slider = $('.slider--width');
 $('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
@@ -11,23 +9,34 @@ $('.slider--controls').style.height = `${$('.slider').clientHeight}px`;
 const sliderControl = $$('.slider--control');
 
 function goPrev(){
-    const slides = $$('.slider--item');
-
-    for(let i = 0; i < (slides.length - 1); i++){
-        slider.append(slides[i]);
-    }
+    updateMargin(2);
 }
 
 function goNext(){
-    const slides = $$('.slider--item');
-    slides[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
-    setTimeout(updateMargin, 300);
+    updateMargin(1);
 }
 
-function updateMargin(){
+function updateMargin(control){
     const slides = $$('.slider--item');
-    slider.appendChild(slides[0]);
-    $$('.slider--item')[slides.length - 1].style.marginLeft = "0px";
+    if(control === 1){
+        slides[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
+        setTimeout(()=>{
+            slider.appendChild(slides[0]);
+            $$('.slider--item')[slides.length - 1].style.marginLeft = "0px";
+        }, 500);
+        
+    }
+    if(control === 2){
+        for(let i = 0; i < (slides.length - 1); i++){
+            slider.append(slides[i]);
+        }
+        $$('.slider--item')[0].style.transition = "none";
+        $$('.slider--item')[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
+        setTimeout(()=>{
+            $$('.slider--item')[0].style.transition = "all ease 0.5s";
+            $$('.slider--item')[0].style.marginLeft = "0px";
+        }, 0.00000000000000000000000000000000000000000001);  
+    }  
 }
 
 sliderControl[0].addEventListener('click', goPrev);
