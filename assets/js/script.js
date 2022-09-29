3b0ecc633539f73accb419211d8a4f40f721fe8e
Daniel Firme <danielfirme.github@gmail.com>
2022-09-28 23:10:40 -0300

    tentando  nova versao do carrossel

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,13 @@ let currentSlide = 0;
 
 let totalSlides = $$('.slider--item').length;
 const slider = $('.slider--width');
-$('.slider--width').style.width = `calc(100vw * ${totalSlides + 1})`;
+$('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
 $('.slider--controls').style.height = `${$('.slider').clientHeight}px`;
 const sliderControl = $$('.slider--control');
 
 function goPrev(){
     const slides = $$('.slider--item');
+
     for(let i = 0; i < (slides.length - 1); i++){
         slider.append(slides[i]);
     }
@@ -20,20 +21,16 @@ function goPrev(){
 function goNext(){
     const slides = $$('.slider--item');
     slides[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
-    slider.appendChild(slides[0]);
-    
-    updateMargin();
+    setTimeout(updateMargin, 300);
 }
 
 function updateMargin(){
-    const newLocal = "0px";
-    $$('.slider--item')[0].style.marginLeft = newLocal;
-    $$('.slider--item')[1].style.marginLeft = `${$('.slider--item').clientWidth}px`;
+    const slides = $$('.slider--item');
+    slider.appendChild(slides[0]);
+    $$('.slider--item')[slides.length - 1].style.marginLeft = "0px";
 }
 
 sliderControl[0].addEventListener('click', goPrev);
 sliderControl[1].addEventListener('click', goNext);
-
-$$('.slider--item')[1].style.marginLeft = `${$$('.slider--item')[0].clientWidth}px`;
 
 //setInterval(goNext, 5000);
